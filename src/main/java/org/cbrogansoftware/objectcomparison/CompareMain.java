eef362902b67b7e9dba969eb76d2c62ac303bcbb
cbrogansoftware <cbrogansoftware@gmail.com>
2022-10-06 15:19:48 -0500

    Version 1.3 for generics problem.

---
+++ src/main/java/org/cbrogansoftware/objectcomparison/CompareMain.java
@@ -0,0 +1,40 @@
+package org.cbrogansoftware.objectcomparison;
+
+public class CompareMain {
+
+    public static void main(String[] args)
+    {
+
+        House house1 = new House(4000, 3.5, "Blue", 2);
+        House house2 = new House(3000, 2.5, "White", 0);
+        House house3 = new House(3000, 2.5, "White", 0);
+
+        System.out.println("----------------------");
+        System.out.println("Three House objects have been created: house1, house2, house3");
+        System.out.println("  house1 is constructed with different parameters than house2 and house3.");
+        System.out.println("  house2 and house3 are constructed with identical parameters.");
+
+        System.out.println("----------------------");
+        System.out.println("Compare house1 to house1. Are they the same by... ");
+        System.out.println("  using reference |" + (house1 == house1) + "|");
+        System.out.println("  using value |" + house1.equals(house1) + "|");
+        System.out.println("  house1 hashCode |" + house1.hashCode() + "|");
+        System.out.println("  house1 hashCode |" + house1.hashCode() + "|");
+
+        System.out.println("----------------------");
+        System.out.println("Compare house1 to house2. Are they the same by... ");
+        System.out.println("  using reference |" + (house1 == house2) + "|");
+        System.out.println("  using value |" + house1.equals(house2) + "|");
+        System.out.println("  house1 hashCode |" + house1.hashCode() + "|");
+        System.out.println("  house2 hashCode |" + house2.hashCode() + "|");
+
+        System.out.println("----------------------");
+        System.out.println("Compare house2 to house3. Are they the same by... ");
+        System.out.println("  using reference |" + (house2 == house3) + "|");
+        System.out.println("  using value |" + house2.equals(house3) + "|");
+        System.out.println("  house2 hashCode |" + house2.hashCode() + "|");
+        System.out.println("  house3 hashCode |" + house3.hashCode() + "|");
+
+    }
+
+}
---
+++ src/main/java/org/cbrogansoftware/objectcomparison/House.java
@@ -0,0 +1,31 @@
+package org.cbrogansoftware.objectcomparison;
+
+import java.util.Objects;
+
+public class House {
+
+    private Integer squareFoot;
+    private Double numBaths;
+    private String exteriorColor;
+    private Integer numFirePlaces;
+
+    public House(Integer squareFoot, Double numBaths, String exteriorColor, Integer numFirePlaces) {
+        this.squareFoot = squareFoot;
+        this.numBaths = numBaths;
+        this.exteriorColor = exteriorColor;
+        this.numFirePlaces = numFirePlaces;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        House house = (House) o;
+        return squareFoot.equals(house.squareFoot) && numBaths.equals(house.numBaths) && exteriorColor.equals(house.exteriorColor) && numFirePlaces.equals(house.numFirePlaces);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(squareFoot, numBaths, exteriorColor, numFirePlaces);
+    }
+}
